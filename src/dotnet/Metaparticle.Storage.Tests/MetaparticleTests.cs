c20cbb29e0957df588b8914bf51f2137ce42bdd3
Srini Vasudevan <srini85@gmail.com>
2018-01-29 20:51:42 +1100

    add more test and remove fluent assertions

--- src/dotnet/Metaparticle.Storage.Tests/MetaparticleTests.cs
+++ src/dotnet/Metaparticle.Storage.Tests/MetaparticleTests.cs
@@ -1,6 +1,5 @@
 using System;
 using Xunit;
-using FluentAssertions;
 
 namespace Metaparticle.Storage.Tests
 {
@@ -13,17 +12,30 @@ namespace Metaparticle.Storage.Tests
             bool testPassed = false;
 
             // act
-           try
-           {
-               var result = Metaparticle.Scoped().Data;
-           }
-           catch
-           {
-               testPassed = true;
-           }
+            try
+            {
+                var result = Metaparticle.Scoped().Data;
+            }
+            catch
+            {
+                testPassed = true;
+            }
             
             // assert
             Assert.True(testPassed, "Didnt throw as expected");
+        }
+
+        [Fact]
+        public void GivenDataDoesntExist_SetData_ValueIsReturned()
+        {
+            // arrange
+            var setValue = "Hello World";
+
+            // act
+            Metaparticle.Scoped().Data = setValue;
+
+            // assert
+            Assert.Equal(setValue, Metaparticle.Scoped().Data);
         }
     }
 }
